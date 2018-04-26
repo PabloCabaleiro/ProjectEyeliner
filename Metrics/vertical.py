fdf6f07655278378458313f7e9cc43919df6fe55
PabloCabaleiro <pcc.sdc@gmail.com>
2018-04-26 18:01:14 +0200

    Corregido error en vertical que devolvía los resultados en negativo

--- Metrics/vertical.py
+++ Metrics/vertical.py
@@ -10,7 +10,7 @@ class VerticalMetrics(object):
     def operate(self,result):
         for i in range(result.lens_start_line, result.lens_end_line + 1):
             if i >= result.cornea_start_line and i <= result.cornea_end_line:
-                self.distances.append(result.get_lens_value(i) - result.get_cornea_value(i))
+                self.distances.append(result.get_cornea_value(i)-result.get_lens_value(i))
             else:
                 self.distances.append(-1)
 
