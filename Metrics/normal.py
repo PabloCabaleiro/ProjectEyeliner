1b26093f25ee37272ce5ac3cfc9c9176c1a40dec
PabloCabaleiro <pcc.sdc@gmail.com>
2018-04-26 18:40:23 +0200

    Test de métricas completos

--- Metrics/normal.py
+++ Metrics/normal.py
@@ -10,31 +10,47 @@ class NormalMetrics(object):
     def operate(self,result):
 
         for i in range(result.lens_start_line, result.lens_end_line+1):
+
+            # Set as negative beacause top of image is 0 so the order in y axis changes
             if i == result.lens_start_line:
-                dy = result.get_lens_value(i+1) - result.get_lens_value(i)
+                dy = (result.get_lens_value(i+1) - result.get_lens_value(i)) * -1
             elif i == result.lens_end_line:
-                dy = result.get_lens_value(i) - result.get_lens_value(i-1)
+                dy = (result.get_lens_value(i) - result.get_lens_value(i-1)) * -1
             else:
-                # We want to set dy = 1 and we know its allways gonna be 2. So div for 2 to normalize.
-                dy = (result.get_lens_value(i + 1) - result.get_lens_value(i - 1)) / 2
+                # We want to set dx = 1 and we know its allways gonna be 2. So div dy for 2 to normalize.
+                dy = (result.get_lens_value(i + 1) - result.get_lens_value(i - 1)) / -2
 
             finish = False
             cornea_values = [y for _,y in result.cornea]
 
             for j in range(min(cornea_values),max(cornea_values)+1):
                 lens_point = result.get_lens_point(i)
-                pos = round((j - lens_point[1]) * dy + i)
+                pos = abs(lens_point[1] - j) * dy + lens_point[0]
 
                 if pos < result.cornea_start_line or pos > result.cornea_end_line:
                     break
 
+                pos = round(pos)
+
                 cornea_point = result.get_cornea_point(pos)
 
                 if cornea_point[1] <= j:
-                    #Tenemos resultado
-                    self.distances.append(utils.get_dist(cornea_point,lens_point))
+                    if (dy > 0 and (pos - 1) < result.cornea_start_line) or (dy <0 and (pos + 1) > result.cornea_end_line):
+                        break
+                    else:
+                        #We got a result
+                        self.distances.append(utils.get_dist(cornea_point,lens_point))
+                        finish = True
+                        break
+                elif dy > 1 and result.get_cornea_value(i+1) <= j:
+                    self.distances.append(utils.get_dist(lens_point,result.get_cornea_point(i+1)))
                     finish = True
                     break
+                elif dy < -1 and result.get_cornea_value(i+1) <= j:
+                    self.distances.append(utils.get_dist(lens_point, result.get_cornea_point(i - 1)))
+                    finish = True
+                    break
+
 
             if not finish:
                 self.distances.append(-1)
--- Metrics/test_metrics.py
+++ Metrics/test_metrics.py
@@ -7,12 +7,12 @@ import math
 
 class TestMetrics(unittest.TestCase):
 
-    data1 = {"lens": [(0,4),(1,4),(2,5),(3,5),(4,5),(5,5),(6,5),(7,5),(8,4),(9,4)],
-             "cornea": [(0,1),(1,1),(2,3),(3,2),(4,1),(5,2),(6,1),(7,3),(8,1),(9,2)]}
-    data2 =  {"lens": [(0,4),(1,4),(2,5),(3,5),(4,5),(5,5),(6,5),(7,5),(8,4),(9,4)],
-              "cornea": [(2,3),(3,2),(4,1),(5,2),(6,1),(7,3),(8,1)]}
-    data3 =  {"lens": [(0,7),(1,6),(2,6),(3,5),(4,3),(5,3),(6,3),(7,5),(8,6),(9,6),(10,7)],
-              "cornea": [(0,3),(1,2),(2,2),(3,1),(4,1),(5,2),(6,2),(7,1),(8,3),(9,4),(10,2)]}
+    data1 = {"lens": [(0,2),(1,2),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,2),(9,2)],
+             "cornea": [(0,5),(1,5),(2,3),(3,4),(4,5),(5,4),(6,5),(7,3),(8,5),(9,4)]}
+    data2 =  {"lens": [(0,2),(1,2),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,2),(9,2)],
+              "cornea": [(2,3),(3,4),(4,5),(5,4),(6,5),(7,3),(8,5)]}
+    data3 =  {"lens": [(0,1),(1,2),(2,2),(3,3),(4,5),(5,5),(6,5),(7,3),(8,2),(9,2),(10,1)],
+              "cornea": [(0,5),(1,6),(2,6),(3,7),(4,7),(5,6),(6,6),(7,7),(8,5),(9,4),(10,6)]}
 
     ############################################NEAREST TEST#########################################################
 
@@ -44,7 +44,7 @@ class TestMetrics(unittest.TestCase):
     ###############################################NORMAL TEST########################################################
 
     def test_normal_normal_case(self):
-        expected_results = [math.sqrt(2),math.sqrt(20),3,4,3,4,math.sqrt(13),math.sqrt(2)]
+        expected_results = [3,math.sqrt(2),math.sqrt(20),3,4,3,4,math.sqrt(13),math.sqrt(2),2]
         result = ResultClass(self.data1["lens"], self.data1["cornea"], True)
         metrics = NormalMetrics(result)
         self.assertEqual(len(expected_results),len(metrics.distances))
@@ -52,7 +52,7 @@ class TestMetrics(unittest.TestCase):
             self.assertAlmostEqual(expected_results[i],metrics.distances[i])
 
     def test_normal_different_sizes(self):
-        expected_results = [-1,math.sqrt(20),3,4,3,4,math.sqrt(13),math.sqrt(2)]
+        expected_results = [-1,-1,math.sqrt(20),3,4,3,4,math.sqrt(13),math.sqrt(2),-1]
         result = ResultClass(self.data2["lens"], self.data2["cornea"], True)
         metrics = NormalMetrics(result)
         self.assertEqual(len(expected_results),len(metrics.distances))
@@ -60,8 +60,8 @@ class TestMetrics(unittest.TestCase):
             self.assertAlmostEqual(expected_results[i],metrics.distances[i])
 
     def test_normal_second_case(self):
-        expected_results = [-1, math.sqrt(13), math.sqrt(13), math.sqrt(5), 1, math.sqrt(2), math.sqrt(5),
-                            math.sqrt(5), math.sqrt(5), -1]
+        expected_results = [-1, -1, math.sqrt(13), math.sqrt(13), math.sqrt(5), 1, 2, math.sqrt(5),
+                            math.sqrt(5), -1, -1]
         result = ResultClass(self.data3["lens"], self.data3["cornea"], True)
         metrics = NormalMetrics(result)
         self.assertEqual(len(expected_results), len(metrics.distances))
