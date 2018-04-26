014f5b0c90b906ef1a8a13e2ea4b8242ec7fba01
PabloCabaleiro <pcc.sdc@gmail.com>
2018-04-26 19:23:59 +0200

    test validate

--- Validation/validate-test.py
+++ Validation/validate-test.py
@@ -1,3 +1,31 @@
 import unittest
+from Validation.validate_configuration import ValidateConfiguration
+from Objects.ResultClass import ResultClass
 
-class TestMetrics(unittest.TestCase):
+
+class ValidationTest(unittest.TestCase):
+    image_list = None;
+    names_list = None;
+
+    def test_get_error(self):
+
+        top1 = top2 = [(i,5) for i in range(30,80)]
+        bot1 = bot2 = [(i,5) for i in range(30,80)]
+        result1 = ResultClass(top1,bot1,3)
+        mse, mae = ValidateConfiguration()._get_error(result1, top2, bot2)
+
+        self.assertEqual(mse,0)
+        self.assertEqual(mae,0)
+
+        bot1 = top1 = [(i,10) for i in range(30,80)]
+        bot2 = [(i,15) for i in range(30,80,5)]
+        top2 = [(i,5) for i in range(30,80,5)]
+        result1 = ResultClass(top1, bot1, 3)
+        mse, mae = ValidateConfiguration()._get_error(result1, top2, bot2)
+
+        self.assertEqual(mse,25)
+        self.assertEqual(mae,5)
+
+
+if __name__ == '__main__':
+    unittest.main()
