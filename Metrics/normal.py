f9c22e3828ef9bd2e49e65a8881589891f7e1433
PabloCabaleiro <pcc.sdc@gmail.com>
2018-05-04 01:28:05 +0200

    Corregido bug en el cálculo de la normal

--- Metrics/normal.py
+++ Metrics/normal.py
@@ -60,14 +60,14 @@ class NormalMetrics(object):
                         points.append(cornea_point)
                         finish = True
                         break
-                elif dy > 1 and result.get_cornea_value(i+1) <= j:
-                    point = result.get_cornea_point(i+1)
+                elif pos < result.cornea_end_line and dy > 1 and result.get_cornea_value(pos+1) <= j:
+                    point = result.get_cornea_point(pos+1)
                     distances.append(utils.get_dist(lens_point,point))
                     points.append(point)
                     finish = True
                     break
-                elif dy < -1 and result.get_cornea_value(i+1) <= j:
-                    point = result.get_cornea_point(i - 1)
+                elif pos > result.cornea_start_line and dy < -1 and result.get_cornea_value(pos-1) <= j:
+                    point = result.get_cornea_point(pos - 1)
                     distances.append(utils.get_dist(lens_point, point))
                     points.append(point)
                     finish = True
@@ -106,9 +106,12 @@ class NormalMetrics(object):
 
             finish = False
             lens_values = [y for _, y in result.lens]
+            max_lens_value = int(max(lens_values))
+            min_lens_value = int(min(lens_values))
+            cornea_point = result.get_cornea_point(i)
 
-            for j in range(int(max(lens_values)), int(min(lens_values)) - 1, -1):
-                cornea_point = result.get_cornea_point(i)
+            for j in range(max_lens_value, min_lens_value - 1, -1):
+
                 pos = abs(cornea_point[1] - j) * dy + cornea_point[0]
 
                 if pos < result.lens_start_line or pos >= result.lens_end_line:
@@ -128,14 +131,14 @@ class NormalMetrics(object):
                         points.append(lens_point)
                         finish = True
                         break
-                elif dy > 1 and result.get_lens_value(i + 1) <= j:
-                    point = result.get_lens_point(i + 1)
+                elif pos < result.lens_end_line and dy > 1 and result.get_lens_value(pos + 1) >= j:
+                    point = result.get_lens_point(pos + 1)
                     distances.append(utils.get_dist(cornea_point, point))
                     points.append(point)
                     finish = True
                     break
-                elif dy < -1 and result.get_lens_value(i + 1) <= j:
-                    point = result.get_lens_point(i - 1)
+                elif pos > result.lens_start_line and dy < -1 and result.get_lens_value(pos - 1) >= j:
+                    point = result.get_lens_point(pos - 1)
                     distances.append(utils.get_dist(cornea_point, point))
                     points.append(point)
                     finish = True
